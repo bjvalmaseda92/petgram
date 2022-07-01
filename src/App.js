c348cae379fe75aeb3d7e7fa4b6e52a0eea22715
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-01 19:23:35 -0400

    :sparkles: [ADD] animations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { CategoryList } from './components/CategoryList'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from './components/styles/GlobalStyles'
 import { PhotoCardList } from './components/PhotoCardList'
 import { Logo } from './components/Logo'
 
 const App = () => {
   return (
     <><GlobalStyles /><Logo /><CategoryList /><PhotoCardList /></>
-
   )
 }
 
