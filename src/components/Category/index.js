b2d8286db8916f826c3807eb4f9b153ab8cc2d4f
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-01 17:52:09 -0400

    :lipstick: [ADD] style category

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Anchor, Image } from './style'
 
 const DEFAULT_COVER = 'https://i.imgur.com/dJa0Hpl.jpg'
 
 const Category = ({ cover = DEFAULT_COVER, path, emoji = '?' }) => {
   return (
-    <a href={path}>
-      <img src={cover} />
+    <Anchor href={path}>
+      <Image src={cover} />
       {emoji}
-    </a>
+    </Anchor>
   )
 }
 
