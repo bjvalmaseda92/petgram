ce7541fe3d7aff043539ad0a602b3331f690d949
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-02 14:06:32 -0400

    :sparkles: [ADD] fetch data from API

--- src/components/PhotoCardList/index.js
+++ src/components/PhotoCardList/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { useQuery, gql } from '@apollo/client'
+
+const whitPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
 
 const PhotoCardList = () => {
+  const { loading, error, data } = useQuery(whitPhotos)
+
+  if (error) {
+    return <h2>Internal Server Error</h2>
+  }
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
   return (
     <ul>
-      {[1, 2, 3, 5, 6, 8, 9, 10].map(item => <PhotoCard key={item} id={item} />)}
+      {data.photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://petgram-bjvalmaseda-server.vercel.app/graphql',
+  cache: new InMemoryCache()
+})
+
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
-root.render(<App />)
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
