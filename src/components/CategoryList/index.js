daf5edc672e6aa3468ffe21b4fcf45a4f7aeb23a
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-01 21:37:31 -0400

    :sparkles: [ADD] categories from de API

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './style'
-import { categories } from '../../../api/db.json'
 
 const CategoryList = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://petgram-bjvalmaseda-server.vercel.app/categories')
+      .then(response => response.json())
+      .then(response => setCategories(response))
+  }, [])
   return (
     <List>{categories.map(category => <Item key={category.id}> <Category {...category} /></Item>)}</List>)
 }
