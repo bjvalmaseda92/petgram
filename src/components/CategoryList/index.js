0b80a4795edd9cbce1c7a497c64e00254a0c5b9d
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-01 22:12:39 -0400

    :sparkles: [ADD] fixed categories list

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -4,13 +4,37 @@ import { Item, List } from './style'
 
 const CategoryList = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
   useEffect(() => {
     window.fetch('https://petgram-bjvalmaseda-server.vercel.app/categories')
       .then(response => response.json())
       .then(response => setCategories(response))
   }, [])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      newShowFixed !== showFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {categories.map(category =>
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>)}
+    </List>)
+
   return (
-    <List>{categories.map(category => <Item key={category.id}> <Category {...category} /></Item>)}</List>)
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  )
 }
 
 export { CategoryList }
