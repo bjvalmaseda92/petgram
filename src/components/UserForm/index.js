3c8fdf47a670b573134d2f423ab700465d7be96f
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-04 23:11:15 -0400

    :sparkles: [ADD] user register mutation

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -5,16 +5,17 @@ import { Button, Form, Input, Title } from './style'
 const UserForm = ({ onSubmit, title }) => {
   const password = useInputValue('')
   const email = useInputValue('')
+
+  const handleForm = (e) => {
+    e.preventDefault()
+    onSubmit({ email: email.value, password: password.value })
+  }
   return (
     <>
       <Title>{title}</Title>
-      <Form onSubmit={(e) => {
-        e.preventDefault()
-        onSubmit()
-      }}
-      >
+      <Form onSubmit={handleForm}>
         <Input type='text' placeholder='Email' {...email} />
-        <Input type='text' placeholder='Password' {...password} />
+        <Input type='password' placeholder='Password' {...password} />
         <Button>{title}</Button>
       </Form>
     </>
--- src/pages/NoRegisterUser.js
+++ src/pages/NoRegisterUser.js
@@ -1,12 +1,21 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../AppContext'
 import { UserForm } from '../components/UserForm'
+import { useRegisterMutation } from '../hooks/useRegisterMutation'
 
 const NoRegisterUser = () => {
   const { activateAuth } = useContext(AppContext)
+  const { registerMutation } = useRegisterMutation()
+
+  const handleRegister = ({ email, password }) => {
+    const input = { email, password }
+    const variables = { input }
+    registerMutation({ variables })
+      .then(activateAuth)
+  }
   return (
     <>
-      <UserForm onSubmit={activateAuth} title='Registrarse' />
+      <UserForm onSubmit={handleRegister} title='Registrarse' />
       <UserForm onSubmit={activateAuth} title='Iniciar sesion' />
     </>
 
