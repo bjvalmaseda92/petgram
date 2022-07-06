86bff9e7f9caa08ce560ebbf399f37a1393803fe
Bábaro Javier Valmaseda Vázquez <bjvalmaseda.g@gmail.com>
2022-07-05 22:40:26 -0400

    :sparkles: [ADD] manifest to PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
 const path = require('path')
 module.exports = {
   entry: './src/index.js',
@@ -18,7 +19,22 @@ module.exports = {
       }
     ]
   },
-  plugins: [new HTMLWebpackPlugin({ template: './public/index.html' })],
+  plugins: [
+    new HTMLWebpackPlugin({ template: './public/index.html' }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Your pet photos app',
+      shortname: 'Petgram -🐱',
+      description: 'Find pet photos and use it as wallpaper',
+      background_color: '#fff',
+      theme_color: '#8d00ff',
+      icons: [
+        {
+          src: path.resolve('public/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: 'maskable'
+        }
+      ]
+    })],
   devServer: {
     historyApiFallback: true
   }
